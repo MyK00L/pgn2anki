6f730f5c27d0098db64b41a8550e12405427fd3c
Michael Chelli <michaelchelli00@gmail.com>
2023-09-04 11:26:20 +0200

    handle multiple instances of the same position, better cli

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use clap::Parser;
 use genanki_rs::*;
 use rand::Rng;
 use sacrifice::prelude::*;
 use sacrifice::{Chess, Position};
-use sacrifice::{Color, File, Rank, Role, Square};
+use sacrifice::{Color, File, Move, Rank, Role, Square};
 use shakmaty::board::Board;
-use std::str::FromStr;
-use std::{env, fs};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+
+use std::fs;
 
 fn build_model() -> Model {
     const MODEL_ID: i64 = 0x25bbbb4805f55380;
@@ -15,12 +19,7 @@ fn build_model() -> Model {
     Model::new_with_options(
         MODEL_ID,
         "Mchess",
-        vec![
-            Field::new("front_board"),
-            Field::new("front_text"),
-            Field::new("back_board"),
-            Field::new("back_text"),
-        ],
+        vec![Field::new("front"), Field::new("back")],
         vec![Template::new("Chess card").qfmt(front_html).afmt(back_html)],
         Some(css),
         None,
@@ -34,9 +33,7 @@ fn board_to_txt(board: &Board, side: Color) -> String {
     let mut utf16 = Vec::<u16>::new();
 
     utf16.push(0xe300);
-    for _ in 0..8 {
-        utf16.push(0xe301);
-    }
+    utf16.resize(utf16.len() + 8, 0xe301);
     utf16.push(0xe302);
     // <br/>
     utf16.push(0x3c);
@@ -86,59 +83,161 @@ fn board_to_txt(board: &Board, side: Color) -> String {
         utf16.push(0x3e);
     }
     utf16.push(0xe305);
-    for _ in 0..8 {
-        utf16.push(0xe306);
-    }
+    utf16.resize(utf16.len() + 8, 0xe306);
     utf16.push(0xe307);
     String::from_utf16(&utf16).expect("error converting board txt from utf16")
 }
 
-fn build_deck(pgn: &str, side: Color) -> Deck {
-    let model = build_model();
-    let mut rng = rand::thread_rng();
-    let mut deck = Deck::new(rng.gen(), "Chess", "chess repertoire deck");
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+struct DeckMapKey {
+    board: Board,
+    side: Color,
+}
+#[derive(Clone, Debug, Default)]
+struct DeckMapVal {
+    answers: HashMap<Board, HashMap<Move, Vec<String>>>,
+    questions: Vec<String>,
+}
+
+fn add_to_map(map: &mut HashMap<DeckMapKey, DeckMapVal>, pgn: &str, side: Color) {
     let start_position = Chess::default();
     let game = sacrifice::read_pgn(pgn);
     let mut stack = vec![game.root()];
     while let Some(node) = stack.pop() {
         if node.board(&start_position).turn() != side {
             if let (Some(prev), Some(mov)) = (node.parent(), node.prev_move()) {
-                let back_board = board_to_txt(node.board(&start_position).board(), side);
-                let comment = node.comment().unwrap_or_default();
-                let front_board = board_to_txt(prev.board(&start_position).board(), side);
-                let back_txt = format!("{}<br/>{}", mov, comment);
-                deck.add_note(
-                    Note::new(
-                        model.clone(),
-                        vec![&front_board, "", &back_board, &back_txt],
-                    )
-                    .expect("error building note"),
-                );
+                let chess0 = prev.board(&start_position);
+                let board0 = chess0.board();
+                let chess1 = node.board(&start_position);
+                let board1 = chess1.board();
+
+                let key = DeckMapKey {
+                    board: board0.clone(),
+                    side,
+                };
+
+                if !map.contains_key(&key) {
+                    map.insert(key.clone(), DeckMapVal::default());
+                }
+
+                let val = map.get_mut(&key).unwrap();
+
+                if !val.answers.contains_key(board1) {
+                    val.answers.insert(board1.clone(), HashMap::new());
+                }
+
+                let answer_val = val.answers.get_mut(board1).unwrap();
+
+                if !answer_val.contains_key(&mov) {
+                    answer_val.insert(mov.clone(), Vec::new());
+                }
+
+                if let Some(comment) = node.comment() {
+                    answer_val.get_mut(&mov).unwrap().push(comment);
+                }
+                // val.questions.push(stuff)
             }
         }
         for i in node.variation_vec() {
             stack.push(i);
         }
     }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+fn add_map_to_deck(deck: &mut Deck, map: &mut HashMap<DeckMapKey, DeckMapVal>) {
+    let model = build_model();
+    for (k, mut v) in map.drain() {
+        let questions_txt = v.questions.join("<br/>\n");
+        let front = format!(
+            r#"<figure>
+                <div class="chess">
+                {}
+                </div>
+                <figcaption>
+                {}
+                </figcaption>
+                </figure>"#,
+            board_to_txt(&k.board, k.side),
+            questions_txt
+        );
+        let back = v
+            .answers
+            .drain()
+            .map(|(aboard, av)| {
+                let txt = av
+                    .iter()
+                    .map(|(mov, comments)| format!("{}<br/>{}", mov, comments.join("<br/>")))
+                    .fold(String::new(), |a, b| a + &b + "<br/>\n");
+                format!(
+                    r#"<figure>
+                        <div class="chess">
+                        {}
+                        </div>
+                        <figcaption>
+                        {}
+                        </figcaption>
+                        </figure>"#,
+                    board_to_txt(&aboard, k.side),
+                    txt
+                )
+            })
+            .fold(String::new(), |a, b| a + &b + "<hr/>\n");
+        deck.add_note(
+            Note::new_with_options(
+                model.clone(),
+                vec![&front, &back],
+                None,
+                Some(vec![&format!("chess::{}", k.side)]),
+                Some(&calculate_hash(&k).to_string()),
+            )
+            .expect("error building note"),
+        );
+    }
+}
+fn gen_deck(white: &[String], black: &[String]) -> Deck {
+    let mut map = HashMap::<DeckMapKey, DeckMapVal>::new();
+    for filename in white {
+        let pgn = fs::read_to_string(filename)
+            .unwrap_or_else(|_| panic!("could not read input pgn file {}", filename));
+        add_to_map(&mut map, &pgn, Color::White);
+    }
+    for filename in black {
+        let pgn = fs::read_to_string(filename)
+            .unwrap_or_else(|_| panic!("could not read input pgn file {}", filename));
+        add_to_map(&mut map, &pgn, Color::Black);
+    }
+    let mut rng = rand::thread_rng();
+    let mut deck = Deck::new(rng.gen(), "Chess", "chess repertoire deck");
+    add_map_to_deck(&mut deck, &mut map);
     deck
 }
 
+/// Convert pgn files into an anki deck
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// filename of the output apkg deck file
+    #[arg(required=true)]
+    out: String,
+    /// filenames of pgns to be processed from white's perspective
+    #[arg(short)]
+    white: Vec<String>,
+    /// filenames of pgns to be  processed from black's perspective
+    #[arg(short)]
+    black: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        eprintln!(
-            "usage: {} pgn_input_filename color apkg_output_filename",
-            args[0]
-        );
-        return;
-    }
-    let pgn = fs::read_to_string(&args[1]).expect("could not read input pgn file");
-    let side =
-        Color::from_str(&args[2]).unwrap_or_else(|_| panic!("{} is not a correct color", args[2]));
-    let deck = build_deck(&pgn, side);
+    let args = Args::parse();
+    let deck = gen_deck(&args.white, &args.black);
     let mut package = Package::new(vec![deck], vec!["ankitemplate/_chess_merida_unicode.ttf"])
         .expect("could not build anki package");
     package
-        .write_to_file(&args[3])
+        .write_to_file(&args.out)
         .expect("could not write anki package");
 }
